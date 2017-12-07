42890207e2e552e6c1e686cd92723a01115cf6b7
Flavio <flvribeiro27@gmail.com>
2017-12-07 07:39:51 -0300

    Template base

---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/dist/css/AdminLTE.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('theme/dist/css/skins/skin-blue.min.css') }}">
+</head>
+<body class="skin-blue">
+    @include('master.includes.header')
+    @include('master.includes.sidebar')
+    
+    <script src="{{ asset('plugins/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('plugins/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('theme/dist/js/adminlte.min.js') }}"></script>
+</body>
+</html>
